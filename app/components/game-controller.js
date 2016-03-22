e941c01b6425f5fb1ca4fdbf3355ae9e1676e70d
Accidental82 <Jason.sullenger@gmail.com>
2016-03-21 20:32:08 -0600

    auto shuffle

--- app/components/game-controller.js
+++ app/components/game-controller.js
@@ -1,6 +1,6 @@
 app.controller('GameController', function ($scope, $timeout) {
     $scope.points = 0;
-    $scope.timer = 60;
+    $scope.timer = 10;
 
     $scope.burrows = [{ id: 1, occupied: false, source: '/assets/img/empty.png' }, { id: 2, occupied: false, source: '/assets/img/empty.png' }, { id: 3, occupied: false, source: '/assets/img/empty.png' }, { id: 4, occupied: false, source: '/assets/img/empty.png' }, { id: 5, occupied: false , source: '/assets/img/empty.png'}, { id: 6, occupied: false, source: '/assets/img/empty.png' }, { id: 7, occupied: false, source: '/assets/img/empty.png' }, { id: 8, occupied: false, source: '/assets/img/empty.png' }, { id: 9, occupied: true, occupant: "Mole", source: '/assets/img/mole.png' }];
 
@@ -11,15 +11,23 @@ app.controller('GameController', function ($scope, $timeout) {
         } else {
            return;
         }
-
-        // victory();
-
-        $timeout(function () {
-            burrow.show = false;
-            shuffle();
-        }, 1000);
-
     }
+    // Relocates Mole
+    $scope.refresh = function(){ 
+        if($scope.timer > 1){
+            $timeout(function () {
+                shuffle();
+                $scope.refresh();
+                $scope.timer--;
+            }, 1000); 
+        }       
+    }
+    // Timer Function
+    // $scope.countdown = function(){
+    //     $timeout(function () {
+    //         $scope.timer -= 1;
+    //     }, 1000);
+    // }
 // Fisher-Yates Shuffle
     function shuffle() {
         var m = $scope.burrows.length
@@ -32,7 +40,7 @@ app.controller('GameController', function ($scope, $timeout) {
             $scope.burrows[i] = t;
         }
     };
-
+// Check Victory
     function victory() {
         if ($scope.points >= 5) {
             $scope.victory = true;
