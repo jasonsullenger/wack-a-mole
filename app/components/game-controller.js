affb81b4c1b3d488366369eb0630ddaec85a441b
Accidental82 <Jason.sullenger@gmail.com>
2016-03-24 12:35:52 -0600

    functionality done

--- app/components/game-controller.js
+++ app/components/game-controller.js
@@ -1,55 +1,88 @@
-app.controller('GameController', function ($scope, $timeout) {
+app.controller('GameController', function($scope, $timeout) {
     $scope.class = "pointerEvent";
+    $scope.startDisable = false;
+    var stopped = false;
     $scope.points = 0;
     $scope.timer = 30;
     var diffy = 1000;
 
-    $scope.burrows = [{ id: 1, occupied: false, source: '/assets/img/empty.png' }, { id: 2, occupied: false, source: '/assets/img/empty.png' }, { id: 3, occupied: false, source: '/assets/img/empty.png' }, { id: 4, occupied: false, source: '/assets/img/empty.png' }, { id: 5, occupied: false , source: '/assets/img/empty.png'}, { id: 6, occupied: false, source: '/assets/img/empty.png' }, { id: 7, occupied: false, source: '/assets/img/empty.png' }, { id: 8, occupied: false, source: '/assets/img/empty.png' }, { id: 9, occupied: true, occupant: "Mole", source: '/assets/img/mole.png' }];
+    $scope.burrows = [{ id: 1, occupied: false, source: '/assets/img/empty.png' }, { id: 2, occupied: false, source: '/assets/img/empty.png' }, { id: 3, occupied: false, source: '/assets/img/empty.png' }, { id: 4, occupied: false, source: '/assets/img/empty.png' }, { id: 5, occupied: false, source: '/assets/img/empty.png' }, { id: 6, occupied: false, source: '/assets/img/empty.png' }, { id: 7, occupied: false, source: '/assets/img/empty.png' }, { id: 8, occupied: false, source: '/assets/img/empty.png' }, { id: 9, occupied: true, occupant: "Mole", source: '/assets/img/mole.png' }];
 
-    $scope.peekaboo = function (burrow) {
-       
+    $scope.peekaboo = function(burrow) {
+
         if (burrow.occupant === "Mole") {
+            document.getElementById('strike').play();
             $scope.points += 100;
             $scope.class = "pointerEvent"
         } else {
-           return;
+            document.getElementById('miss').play();
+            return;
         }
     }
-    
 
-  $scope.pointerEvent = function(){
-    if ($scope.class === "pointerEvent")
-      $scope.class = "enabled";
-    else
-      $scope.class = "enabled";
-  };
-    // Relocates Mole
-    $scope.refresh = function(){
+    // Prevents double clicking mole
+    $scope.pointerEvent = function() {
+        if ($scope.class === "pointerEvent"){
+            $scope.class = "enabled";
+        }
+    };
+            
+            
+
+    // Game start
+    $scope.start = function() {
         $scope.pointerEvent();
-        if($scope.timer > 0){
-            $timeout(function () {
+        $scope.startDisable = "pointerEvent"
+        stopped = false;
+        countDown();
+        refresh();
+    }
+    // Game Reset
+    $scope.reset = function() {
+        stopped = true;
+        $scope.class = "pointerEvent";
+        $scope.startDisable = null;
+        $scope.victory = false;
+        $scope.points = 0;
+        var diffy = 1000;
+        $scope.timer = 30;
+    }
+    // Relocate Mole
+    function refresh() {
+        if ($scope.timer > 0 && stopped === false) {
+            $timeout(function() {
+                $scope.pointerEvent();
                 shuffle();
-                $scope.timer--;
-                $scope.refresh();
-            }, diffy); 
-        }else{
+                refresh();
+            }, diffy);
+        } else if($scope.timer < 1){
             victory();
         }
     }
+
     // Sets difficulty
-    $scope.difficulty = function(x){
-        if(x == 'Easy'){
+    $scope.difficulty = function(x) {
+        if (x == 'Easy') {
             diffy = 1000;
         }
-        if(x == 'Extreme'){
-            diffy = 600;
+        if (x == 'Extreme') {
+            diffy = 475;
         }
-        else{
-            diffy = 800;
+        else {
+            diffy = 650;
         }
         return diffy;
     }
-// Fisher-Yates Shuffle
+    // Timer countdown
+    function countDown() {
+        if ($scope.timer > 0 && stopped === false) {
+            $timeout(function() {
+                $scope.timer--;
+                countDown();
+            }, 1000);
+        }
+    }
+    // Fisher-Yates Shuffle
     function shuffle() {
         var m = $scope.burrows.length
         var t;
@@ -61,15 +94,12 @@ app.controller('GameController', function ($scope, $timeout) {
             $scope.burrows[i] = t;
         }
     };
-// Check Victory
+ 
+    // Check Victory
     function victory() {
         $scope.victory = true;
-        
-        // if ($scope.points >= 5) {
-        //     document.getElementById('winner').play();
-        // } else if ($scope.points <= -5) {
-        //     $scope.defeat = true;
-        // }
+        document.getElementById('winner').play();
     };
 
 })
+
